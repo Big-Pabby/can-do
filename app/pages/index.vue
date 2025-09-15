18619422db3549b6d3188845516d883a741ac54b
BigPabby <victoradekunle312@gmail.com>
2025-09-15 23:51:11 +0100

    fixes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="space-y-4">
-     <div class="px-5 pt-16 text-white space-y-2">
+    <div class="px-5 pt-16 text-white space-y-2">
       <h4 class="font-bold text-xl">Lived Expert</h4>
       <p class="text-sm">
         Suffolk Follow Up Group Information Sheet & Consent Form
       </p>
     </div>
-    <About v-if="onboard === 'about'" :onToggle="toggleScreen"/>
-    <UserInfo v-if="onboard === 'info'" :onToggle="toggleScreen"/>
+    <About v-if="onboard === 'about'" :onToggle="toggleScreen" />
+    <UserInfo v-if="onboard === 'info'" :onToggle="toggleScreen" />
     <Submitted v-if="onboard === 'submit'" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import About from './components/About.vue'
-import UserInfo from './components/UserInfo.vue'
-import Submitted from './components/Submitted.vue'
-const onboard = ref<string>('about')
+import { ref } from "vue";
+import About from "./components/About.vue";
+import UserInfo from "./components/UserInfo.vue";
+import Submitted from "./components/Submitted.vue";
+const onboard = ref<string>("about");
 const toggleScreen = (name: string) => {
-    onboard.value = name
-}
+  onboard.value = name;
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 </script>
 
 <style></style>
