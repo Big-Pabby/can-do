e631d9483149941c633767abe1d9ec509ca29fa6
BigPabby <victoradekunle312@gmail.com>
2025-10-23 15:03:12 +0100

    fixes

--- app/components/Map.vue
+++ app/components/Map.vue
@@ -226,15 +226,11 @@ onMounted(async () => {
     zoomControl: false,
   }).setView([51.5074, -0.1278], 13);
 
-  window.L.tileLayer(
-    "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: "abcd",
-      maxZoom: 19,
-    }
-  ).addTo(map);
+  window.L.tileLayer("https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}", {
+    attribution: '&copy; <a href="https://www.google.com/maps">Google Maps</a>',
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }).addTo(map);
 
   // Add service markers
   props.services.forEach((service) => {
